ba9f4dbd4740726f107596a6810b17741dff38a1
abdelrahman mousa <abdelrahmanmysara33@gmail.com>
2021-08-07 16:31:52 +0200

    fix:swagger documentation

--- src/notification/model/notification.model.ts
+++ src/notification/model/notification.model.ts
@@ -1,6 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 import { ObjectID } from 'mongodb';
+import { ApiProperty } from '@nestjs/swagger';
 
 export type NotificationDocument = Notification & Document;
 
@@ -9,15 +10,19 @@ export class Notification {
   _id?: ObjectID;
 
   @Prop()
+  @ApiProperty()
   mobile?: string;
 
   @Prop()
+  @ApiProperty()
   token?: string;
 
   @Prop()
+  @ApiProperty()
   title?: string;
 
   @Prop({ required: true, trim: true })
+  @ApiProperty()
   body: string;
 }
 
