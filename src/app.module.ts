625cbab9119079dd5f2cf3ab7a139ca2083a4346
abdelrahman mousa <abdelrahmanmysara33@gmail.com>
2021-08-07 15:17:46 +0200

    feat: applied throttling

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,18 @@ import { Module } from '@nestjs/common';
 import { DataBaseModule } from './_common/database/database.module';
 import { NotificationModule } from './notification/notification.module';
 import { UserModule } from './user/user.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [DataBaseModule, NotificationModule, UserModule],
+  imports: [
+    // used for throttling requests cause the number of requests per minute is limited
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
+    DataBaseModule,
+    NotificationModule,
+    UserModule,
+  ],
 })
 export class AppModule {}
